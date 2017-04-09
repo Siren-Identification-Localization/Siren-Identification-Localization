7175282378e06030166a8df7562248cfcc98ad6c
Leonard Yulianus <leonard.yulianus@gmail.com>
2017-04-09 00:36:10 -0400

    Complete training script

---
+++ train.py
@@ -0,0 +1,37 @@
+import numpy as np
+import scipy.signal as signal
+from pickle import dump
+from scipy.io import wavfile
+from argparse import ArgumentParser
+from sklearn.decomposition import NMF
+from sklearn.preprocessing import minmax_scale
+
+if __name__ == '__main__':
+    # Config
+    num_of_basis = 3
+    stft_segment = 256
+
+    # Read arguments
+    parser = ArgumentParser()
+    parser.add_argument('model', help='model output')
+    parser.add_argument('input', nargs='+', help='audio file input(s)')
+    args = parser.parse_args()
+
+    models = []
+
+    model = NMF(n_components=num_of_basis)
+
+    for file in args.input:
+        rate, sample = wavfile.read(file)
+
+        f, t, Zxx = signal.stft(sample, fs=rate, nperseg=stft_segment, window='hamming')
+        power_spectrogram = minmax_scale(np.power(np.absolute(Zxx), 2))
+
+        model.fit(power_spectrogram.T)
+        models.append(model.components_)
+
+    mean_H = np.mean(models, axis=0)
+
+    # Save model
+    with open(args.model, 'wb') as f:
+        dump(mean_H, f)
