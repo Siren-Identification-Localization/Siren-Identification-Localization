e18b93861755ab3b8ab3be4078535879bdd47835
Hankyu Jang <jhkmath@gmail.com>
2017-05-03 22:35:31 -0400

    Add file to draw three wavplots fed into three microphones

--- draw_wavplots.py
+++ draw_wavplots.py
@@ -1,5 +1,5 @@
-import matplotlib as mpl
-mpl.use('Agg')
+#  import matplotlib as mpl
+#  mpl.use('Agg')
 import matplotlib.pyplot as plt
 import math
 import numpy as np
@@ -25,7 +25,27 @@ if __name__ == '__main__':
     sampling_rate2, data2 = scipy.io.wavfile.read(args.infile2)
     sampling_rate3, data3 = scipy.io.wavfile.read(args.infile3)
 
-    plot_wave(data1[100:200], "Wave recorded in microphone 1", "time step", "frequency", "plots/microphone1.png")
-    plot_wave(data2[100:200], "Wave recorded in microphone 2", "time step", "frequency", "plots/microphone2.png")
-    plot_wave(data3[100:200], "Wave recorded in microphone 3", "time step", "frequency", "plots/microphone3.png")
+    #  plot_wave(data1[100:200], "Wave recorded in microphone 1", "time step", "frequency", "plots/microphone1.png")
+    #  plot_wave(data2[100:200], "Wave recorded in microphone 2", "time step", "frequency", "plots/microphone2.png")
+    #  plot_wave(data3[100:200], "Wave recorded in microphone 3", "time step", "frequency", "plots/microphone3.png")
     
+    plt.subplot(3, 1, 1)
+    plt.plot(data1[100:400], 'r')
+    plt.title("Wave recordings when ambulance is 10m behind")
+    plt.ylabel("Microphone 1")
+    plt.locator_params(axis = 'y', nbins = 5)
+
+    plt.subplot(3, 1, 2)
+    plt.plot(data2[100:400], 'g')
+    plt.ylabel("Microphone 2")
+    plt.locator_params(axis = 'y', nbins = 5)
+    
+    plt.subplot(3, 1, 3)
+    plt.plot(data3[100:400], 'b')
+    plt.ylabel("Microphone 3")
+    plt.locator_params(axis = 'y', nbins = 5)
+    plt.xlabel("time (1/44100 s)")
+    
+    plt.savefig("plots/microphone_recordings.png")
+    plt.show()
+
